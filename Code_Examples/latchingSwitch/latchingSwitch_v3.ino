7bc9e071d1c9449bc2e4b2f79fc2662d41b63462
Andre Simao <111400782+andresima0@users.noreply.github.com>
2023-10-20 02:57:33 -0300

    Update latchingSwitch_v3.ino

--- Code_Examples/latchingSwitch/latchingSwitch_v3.ino
+++ Code_Examples/latchingSwitch/latchingSwitch_v3.ino
@@ -1,4 +1,6 @@
-/*Esse código implementa uma latching switch que envia status da porta via Bluetooth, sendo:
+/*Esse código consiste em uma latching switch que ativa portas físicas de um ESP32.
+É apresentado o estado atual da porta ativada via requisições Bluetooth, para algum aplicativo de visualização.
+Regra de funcionamento:
   porta 25 desligada = 100;
   porta 25 ligada = 101;
   porta 26 desligada = 200;
@@ -9,7 +11,7 @@
 
 #include <BLEDevice.h>
 #include <BLEServer.h>
-#include <BLEUtils.h>
+#include <BLECharacteristic.h>
 
 const int buttonPin = 12;
 const int outputPins[] = {25, 26, 27};
@@ -19,52 +21,36 @@ int lastButtonState = HIGH;
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
 bool outputState[] = {false, false, false};
-
-BLEServer* pServer = NULL;
-BLECharacteristic* pCharacteristic = NULL;
-
-class CharacteristicCallbacks : public BLECharacteristicCallbacks {
-  void onWrite(BLECharacteristic* pCharacteristic) {
-    std::string value = pCharacteristic->getValue();
-    String statusMessage = "porta " + String(outputPins[currentOutput]) + " ";
-    
-    if (value == "1") {
-      outputState[currentOutput] = true;
-      digitalWrite(outputPins[currentOutput], HIGH);
-      statusMessage += "ligada = " + String(100 + outputPins[currentOutput]);
-    } else if (value == "0") {
-      outputState[currentOutput] = false;
-      digitalWrite(outputPins[currentOutput], LOW);
-      statusMessage += "desligada = " + String(100 + outputPins[currentOutput]);
-    }
-
-    currentOutput = (currentOutput + 1) % 3;
-    pCharacteristic->setValue(statusMessage.c_str());
-    pCharacteristic->notify();
-  }
-};
+BLEServer *pServer = NULL;
+BLECharacteristic *pCharacteristic = NULL;
 
 void setup() {
-  BLEDevice::init("ESP32_BLE_Switch");
-  pServer = BLEDevice::createServer();
-  
-  BLEService* pService = pServer->createService(BLEUUID("abcd1234-ab12-cd34-a123-456789abcdef"));
-  pCharacteristic = pService->createCharacteristic(BLEUUID("abcd1234-ab12-cd34-a123-456789abcdef"), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
-
-  pCharacteristic->setCallbacks(new CharacteristicCallbacks());
-  pService->start();
-  BLEAdvertising* pAdvertising = pServer->getAdvertising();
-  pAdvertising->start();
-
   for (int i = 0; i < 3; i++) {
     pinMode(outputPins[i], OUTPUT);
   }
   pinMode(buttonPin, INPUT_PULLUP);
+
+  // Inicializa o dispositivo BLE com um nome
+  BLEDevice::init("ESP32_BLE_Switch");
+
+  // Cria um servidor BLE e um serviço
+  pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(BLEUUID("abcd1234-ab12-cd34-a123-456789abcdef")); // Substitua pelo seu UUID de serviço
+
+  // Cria uma característica BLE com propriedades de leitura
+  pCharacteristic = pService->createCharacteristic(
+    BLEUUID("abcd1234-ab12-cd34-a123-456789abcdef"), BLECharacteristic::PROPERTY_READ
+  ); // Substitua pelo seu UUID de característica
+
+  // Inicia o serviço e a publicidade BLE
+  pService->start();
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
 }
 
 void loop() {
   int reading = digitalRead(buttonPin);
-  
+
   if (reading != lastButtonState) {
     lastDebounceTime = millis();
   }
@@ -74,13 +60,22 @@ void loop() {
       buttonState = reading;
 
       if (buttonState == LOW) {
+        // Se o botão está pressionado, alterne a saída
         outputState[currentOutput] = !outputState[currentOutput];
         digitalWrite(outputPins[currentOutput], outputState[currentOutput]);
-        int statusValue = outputState[currentOutput] ? 101 : 100;
-        String statusMessage = "porta " + String(outputPins[currentOutput]) + " " + (outputState[currentOutput] ? "ligada = " : "desligada = ") + String(statusValue);
-        pCharacteristic->setValue(statusMessage.c_str());
-        pCharacteristic->notify();
+
+        // Desligue a saída anterior
+        int previousOutput = (currentOutput + 2) % 3;
+        outputState[previousOutput] = false;
+        digitalWrite(outputPins[previousOutput], outputState[previousOutput]);
+
+        // Atualize a saída atual para a próxima
         currentOutput = (currentOutput + 1) % 3;
+
+        // Atualiza o valor da característica BLE e notifica com números
+        int outputStatus = (outputPins[currentOutput] * 100) + (outputState[currentOutput] ? 1 : 0);
+        pCharacteristic->setValue(outputStatus);
+        pCharacteristic->notify();
       }
     }
   }
